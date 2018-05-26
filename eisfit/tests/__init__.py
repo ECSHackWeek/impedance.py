83900f342888724a728ec47072baa7d2c09656b2
Matt Murbach <mmurbach@uw.edu>
2018-05-25 17:03:23 -0700

    add tests for rmse

--- eisfit/tests/__init__.py
+++ eisfit/tests/__init__.py
@@ -1,4 +1,4 @@
 __version__ = "0.1"
-from .test_circuit_elements import *  # noqa: F401, F403
-from .test_circuits import *  # noqa: F401, F403
-from .test_fitting import *  # noqa: F401, F403
+# from .test_circuit_elements import *  # noqa: F401, F403
+# from .test_circuits import *  # noqa: F401, F403
+# from .test_fitting import *  # noqa: F401, F403
--- eisfit/tests/test_fitting.py
+++ eisfit/tests/test_fitting.py
@@ -1,5 +1,5 @@
-from eisfit.fitting import buildCircuit
-
+from eisfit.fitting import buildCircuit, rmse
+import numpy as np
 
 # def test_residuals():
 #     pass
@@ -24,3 +24,15 @@ def test_buildCircuit():
         'p((R([0.01],[1000.0,1.0,0.01]),' + \
         '([15,0.9],[1000.0,1.0,0.01]))),' + \
         'W([1,1000],[1000.0,1.0,0.01])])'
+
+
+def test_RMSE():
+    a = np.array([2 + 4*1j, 3 + 2*1j])
+    b = np.array([2 + 4*1j, 3 + 2*1j])
+
+    assert rmse(a, b) == 0.0
+
+    c = np.array([2 + 4*1j, 1 + 4*1j])
+    d = np.array([4 + 2*1j, 3 + 2*1j])
+
+    assert rmse(c, d) == 2*np.sqrt(2)
