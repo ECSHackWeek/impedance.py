7081dcd2c290c12d30745fc75510521c58c35ce1
Matt Murbach <mmurbach@uw.edu>
2018-06-15 16:57:42 -0700

    add tests for plotting

---
+++ impedance/tests/test_plotting.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from impedance.plotting import plot_nyquist
+
+
+def test_plot_nyquist():
+
+    frequencies = [1000.0, 1.0, 0.01]
+    Z = np.array([1, 2, 3]) + 1j*np.array([2, 3, 4])
+
+    fig, ax = plt.subplots()
+    ax = plot_nyquist(ax, frequencies, Z)
+
+    xs, ys = ax.lines[0].get_xydata().T
+
+    assert (xs == Z.real).all() and (ys == -Z.imag).all()
