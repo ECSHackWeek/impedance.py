ad8c9046ed1ff1732fe877788130bf76acd88979
Lok-kun Tsui <lktsui@unm.edu>
2018-10-02 11:30:36 -0500

    Data export format is dictionary instead of list for circuit models

--- impedance/model_io.py
+++ impedance/model_io.py
@@ -8,10 +8,22 @@ def model_export(model, filepath):
 
     model_string = model.circuit
 
+    model_name = model.name
+
     model_param_names = model.get_param_names()
 
     model_params = [(name, model.parameters_[index])
                     for index, name in enumerate(model_param_names)]
+
+
+    if not model_name:
+        model_name = "None"
+
+    data_dict = {"Name":model_name,
+                 "Circuit String":model_string,
+                 "Parameters":model_params
+
+    }
 
 
     print("Exporting the following model to destination %s"%filepath)
@@ -19,7 +31,7 @@ def model_export(model, filepath):
 
     destination_object = open(filepath, 'w')
 
-    json.dump([model_string, model_params], destination_object)
+    json.dump(data_dict, destination_object)
 
 
 def model_import(filepath):
@@ -31,13 +43,13 @@ def model_import(filepath):
 
     json_data = json.load(json_data_file)
 
-    circuit_string = json_data[0]
-
-    circuit_param_list = json_data[1]
+    circuit_name = json_data["Name"]
+    circuit_string = json_data["Circuit String"]
+    circuit_param_list = json_data["Parameters"]
 
     circuit_params = [item[1] for item in circuit_param_list]
 
-    circuit_model = CustomCircuit(initial_guess=circuit_params, circuit=circuit_string)
+    circuit_model = CustomCircuit(initial_guess=circuit_params, circuit=circuit_string, name=circuit_name)
     circuit_model.parameters_ = np.array(circuit_model.initial_guess)
 
     print("Imported model from %s with the following circuit parameters"%filepath)
