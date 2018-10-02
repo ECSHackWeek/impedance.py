5f2657c9aa76a4d3b347fed81eb9311f84c4cd0c
Lok-kun Tsui <lktsui@unm.edu>
2018-10-02 10:35:06 -0500

    Added ability to import JSON data and construct a model from it

--- impedance/model_io.py
+++ impedance/model_io.py
@@ -1,8 +1,8 @@
 import json
-
+from .circuits import CustomCircuit
 
 def model_export(model, filepath):
-    """Export the impedance model to a .py file"""
+    """Export the impedance model to a JSON file"""
 
 
     model_string = model.circuit
@@ -18,4 +18,27 @@ def model_export(model, filepath):
     destination_object = open(filepath, 'w')
 
     json.dump([model_string, model_params], destination_object)
+
+
+def model_import(filepath):
+
+    """Import the file from JSON and construct a circuit element from it"""
+
+    json_data_file = open(filepath, 'r')
+
+
+    json_data = json.load(json_data_file)
+
+    circuit_string = json_data[0]
+
+    circuit_param_list = json_data[1]
+
+    circuit_params = [item[1] for item in circuit_param_list]
+
+    circuit_model = CustomCircuit(initial_guess=circuit_params, circuit=circuit_string)
+
+    "Imported model with the following circuit parameters"
+    print(circuit_model)
+
+    return circuit_model
 
