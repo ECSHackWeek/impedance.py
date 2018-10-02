df74fbbe5a6f248d9e2bfb696f4362233cb92448
Lok-kun Tsui <lktsui@unm.edu>
2018-10-02 12:19:16 -0500

    Separate the initial guess and fitted parameters in import export

--- impedance/model_io.py
+++ impedance/model_io.py
@@ -12,6 +12,8 @@ def model_export(model, filepath):
 
     model_param_names = model.get_param_names()
 
+    model_initial_guess = [(name, model.initial_guess[index])
+                    for index, name in enumerate(model_param_names)]
     model_params = [(name, model.parameters_[index])
                     for index, name in enumerate(model_param_names)]
 
@@ -21,10 +23,9 @@ def model_export(model, filepath):
 
     data_dict = {"Name":model_name,
                  "Circuit String":model_string,
+                 "Initial Guess":model_initial_guess,
                  "Parameters":model_params
-
     }
-
 
     print("Exporting the following model to destination %s"%filepath)
     print(model)
@@ -34,9 +35,23 @@ def model_export(model, filepath):
     json.dump(data_dict, destination_object)
 
 
-def model_import(filepath):
+def model_import(filepath, as_initial_guess = True):
+    """ Imports parameters of a model from JSON
 
-    """Import the file from JSON and construct a circuit element from it"""
+    Parameters
+    ---------
+
+    as_initial_guess: bool
+        If True, imports the fitted parameters from json as an unfitted model
+        otherwise imports the data as a fitted model object
+
+    Returns
+    ----------
+    circuit_model: CustomCircuit
+        Circuit model object
+
+
+    """
 
     json_data_file = open(filepath, 'r')
 
@@ -46,11 +61,13 @@ def model_import(filepath):
     circuit_name = json_data["Name"]
     circuit_string = json_data["Circuit String"]
     circuit_param_list = json_data["Parameters"]
+    circuit_ig_list = json_data["Initial Guess"]
 
     circuit_params = [item[1] for item in circuit_param_list]
+    circuit_initial_guess = [item[1] for item in circuit_ig_list]
 
-    circuit_model = CustomCircuit(initial_guess=circuit_params, circuit=circuit_string, name=circuit_name)
-    circuit_model.parameters_ = np.array(circuit_model.initial_guess)
+    circuit_model = CustomCircuit(initial_guess=circuit_initial_guess, circuit=circuit_string, name=circuit_name)
+    circuit_model.parameters_ = np.array(circuit_params)
 
     print("Imported model from %s with the following circuit parameters"%filepath)
     print(circuit_model)
