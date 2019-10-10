a8de44d633d7713ae764a59c7453d2a0cc4f0bfa
Matt Murbach <mdmurbach@users.noreply.github.com>
2019-10-10 11:31:06 -0700

    Bump the docs version up to 0.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, impedance.py developers'
 author = ''
 
 # The short X.Y version
-version = '0.2'
+version = '0.4'
 # The full version, including alpha/beta/rc tags
-release = '0.2-beta'
+release = '0.4-beta'
 
 
 # -- General configuration ---------------------------------------------------
