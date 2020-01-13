ba2fc718dc94ebdca75d02aa69f08549b4a791be
Matt Murbach <matt@hivebattery.com>
2020-01-13 08:56:36 -0800

    bump to version 0.5.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = ''
 # The short X.Y version
 version = '0.5'
 # The full version, including alpha/beta/rc tags
-release = '0.5'
+release = '0.5.1'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="impedance",
-    version="0.5.0",
+    version="0.5.1",
     author="impedance.py developers",
     author_email="matt.murbach@gmail.com",
     description="A Python package for working with impedance data",
