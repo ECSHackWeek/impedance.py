5d5e42ef90d1474fedf37ad11b8250422debc903
Matt Murbach <matt@hivebattery.com>
2020-02-14 08:12:29 -0800

    add test for plot_altair

--- impedance/tests/test_plotting.py
+++ impedance/tests/test_plotting.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt  # noqa E402
 import numpy as np
-from impedance.plotting import plot_nyquist
+from impedance.plotting import plot_nyquist, plot_altair
+import json
 
 
 def test_plot_nyquist():
@@ -13,3 +14,16 @@ def test_plot_nyquist():
     xs, ys = ax.lines[0].get_xydata().T
 
     assert (xs == Z.real).all() and (ys == -Z.imag).all()
+
+
+def test_plot_altair():
+    frequencies = [1000.0, 1.0, 0.01]
+    Z = np.array([1, 2, 3]) + 1j*np.array([2, 3, 4])
+
+    chart = plot_altair({'data': {'f': frequencies, 'Z': Z},
+                         'fit': {'f': frequencies, 'Z': Z, 'fmt': '-'}},
+                        size=400)
+
+    datasets = json.loads(chart.to_json())['datasets']
+    for dataset in datasets.keys():
+        assert len(datasets[dataset]) == len(Z)
