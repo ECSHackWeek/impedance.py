8a29e65099279fc489f1f54892327dee67328e50
Brian Gerwe <38819321+BGerwe@users.noreply.github.com>
2020-02-14 16:16:34 -0800

    More plotting options in plot_nyquist (#78)
    * Added kwargs to plot_nyquist for matplotlib kwargs. Removed  arg from plot_nyquist and changed calls in other areas to reflect the change.
    
    * Fixed randles.plot testing

--- impedance/tests/test_plotting.py
+++ impedance/tests/test_plotting.py
@@ -5,11 +5,10 @@ from impedance.plotting import plot_nyquist
 
 def test_plot_nyquist():
 
-    frequencies = [1000.0, 1.0, 0.01]
     Z = np.array([1, 2, 3]) + 1j*np.array([2, 3, 4])
 
     _, ax = plt.subplots()
-    ax = plot_nyquist(ax, frequencies, Z)
+    ax = plot_nyquist(ax, Z)
 
     xs, ys = ax.lines[0].get_xydata().T
 
