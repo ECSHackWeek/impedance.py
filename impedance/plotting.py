fix units of bode plot phase

impedance/plotting.py
@@ -108,7 +108,7 @@ def plot_bode(axes, f, Z, scale=1, units='Ohms', fmt='.-', **kwargs):
     ax_mag, ax_phs = axes
 
     ax_mag.plot(f, np.abs(Z), fmt, **kwargs)
-    ax_phs.plot(f, -np.angle(Z), fmt, **kwargs)
+    ax_phs.plot(f, -np.angle(Z, deg=True), fmt, **kwargs)
 
     # Set the y-axis labels
     ax_mag.set_ylabel(r'$|Z(\omega)|$ ' +
@@ -207,7 +207,7 @@ def plot_altair(data_dict, size=400):
             height=size/2 - 25
         ).transform_calculate(
             mag="sqrt(pow(datum.z_real,2) + pow(datum.z_imag,2))",
-            neg_phase="-atan(datum.z_imag/datum.z_real)"
+            neg_phase="-(180/PI)*atan(datum.z_imag/datum.z_real)"
         )
 
         bode_mag = bode.encode(y=alt.Y('mag:Q',
@@ -252,7 +252,7 @@ def plot_altair(data_dict, size=400):
             height=size/2 - 25
         ).transform_calculate(
             mag="sqrt(pow(datum.z_real,2) + pow(datum.z_imag,2))",
-            neg_phase="-atan(datum.z_imag/datum.z_real)"
+            neg_phase="-(180/PI)*atan(datum.z_imag/datum.z_real)"
         ).interactive()
 
         bode_mag = bode.encode(y=alt.Y('mag:Q',

impedance/tests/test_plotting.py
@@ -16,7 +16,7 @@ def test_plot_bode():
     assert (xs == f).all() and (ys == np.abs(Z)).all()
 
     xs, ys = axes[1].lines[0].get_xydata().T
-    assert (xs == f).all() and (ys == -np.angle(Z)).all()
+    assert (xs == f).all() and (ys == -np.angle(Z, deg=True)).all()
 
 
 def test_plot_nyquist():