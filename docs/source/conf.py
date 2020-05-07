7c584a42e8c437052b1bf8b74ae13a17214ab6e4
khuynh <hkennyv@gmail.com>
2020-05-07 09:26:49 -0700

    bump version for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, impedance.py developers'
 author = ''
 
 # The short X.Y version
-version = '0.5'
+version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '0.5.1'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
