b2ea8701b3e720ef0fc4cf820cf5436f4aa7acbd
Marcus Karlstad <marcus.karlstad@saftbatteries.com>
2022-09-21 08:57:33 +0200

    Added replace(",", ".") in the readGamry function

--- impedance/preprocessing.py
+++ impedance/preprocessing.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 def readFile(filename, instrument=None):
-    """ A wrapper for reading in many common types of impedance files
+    """A wrapper for reading in many common types of impedance files
 
     Parameters
     ----------
@@ -24,29 +24,37 @@ def readFile(filename, instrument=None):
 
     """
 
-    supported_types = ['gamry', 'autolab', 'parstat', 'zplot', 'versastudio',
-                       'powersuite', 'biologic', 'chinstruments']
+    supported_types = [
+        "gamry",
+        "autolab",
+        "parstat",
+        "zplot",
+        "versastudio",
+        "powersuite",
+        "biologic",
+        "chinstruments",
+    ]
 
     if instrument is not None:
-        assert instrument in supported_types,\
-            '{} is not a supported instrument ({})'.format(instrument,
-                                                           supported_types)
+        assert (
+            instrument in supported_types
+        ), "{} is not a supported instrument ({})".format(instrument, supported_types)
 
-    if instrument == 'gamry':
+    if instrument == "gamry":
         f, Z = readGamry(filename)
-    elif instrument == 'autolab':
+    elif instrument == "autolab":
         f, Z = readAutolab(filename)
-    elif instrument == 'biologic':
+    elif instrument == "biologic":
         f, Z = readBioLogic(filename)
-    elif instrument == 'parstat':
+    elif instrument == "parstat":
         f, Z = readParstat(filename)
-    elif instrument == 'zplot':
+    elif instrument == "zplot":
         f, Z = readZPlot(filename)
-    elif instrument == 'versastudio':
+    elif instrument == "versastudio":
         f, Z = readVersaStudio(filename)
-    elif instrument == 'powersuite':
+    elif instrument == "powersuite":
         f, Z = readPowerSuite(filename)
-    elif instrument == 'chinstruments':
+    elif instrument == "chinstruments":
         f, Z = readCHInstruments(filename)
     elif instrument is None:
         f, Z = readCSV(filename)
@@ -55,7 +63,7 @@ def readFile(filename, instrument=None):
 
 
 def readGamry(filename):
-    """ function for reading the .DTA file from Gamry
+    """function for reading the .DTA file from Gamry
 
     Parameters
     ----------
@@ -71,24 +79,25 @@ def readGamry(filename):
 
     """
 
-    with open(filename, 'r', encoding='ISO-8859-1') as input_file:
+    with open(filename, "r", encoding="ISO-8859-1") as input_file:
         lines = input_file.readlines()
 
     end_line = 0
 
     for i, line in enumerate(lines):
-        if 'ZCURVE' in line:
+        if "ZCURVE" in line:
             start_line = i
-        if 'EXPERIMENTABORTED' in line:
+        if "EXPERIMENTABORTED" in line:
             end_line = i
 
     if end_line != 0:
-        raw_data = lines[start_line + 3:end_line]
+        raw_data = lines[start_line + 3 : end_line]
     else:
-        raw_data = lines[start_line + 3:]
+        raw_data = lines[start_line + 3 :]
 
     f, Z = [], []
     for line in raw_data:
+        line = line.replace(",", ".")
         each = line.split()
         f.append(float(each[2]))
         Z.append(complex(float(each[3]), float(each[4])))
@@ -97,7 +106,7 @@ def readGamry(filename):
 
 
 def readAutolab(filename):
-    """ function for reading comma-delimited files from Autolab
+    """function for reading comma-delimited files from Autolab
 
     Parameters
     ----------
@@ -113,17 +122,17 @@ def readAutolab(filename):
 
     """
 
-    with open(filename, 'r', encoding="utf8") as input_file:
+    with open(filename, "r", encoding="utf8") as input_file:
         lines = input_file.readlines()
 
     for i, line in enumerate(lines):
-        if line.find('Freq') != -1:
+        if line.find("Freq") != -1:
             start_line = i
 
-    raw_data = lines[start_line+1:]
+    raw_data = lines[start_line + 1 :]
     f, Z = [], []
     for line in raw_data:
-        each = line.split(',')
+        each = line.split(",")
         f.append(float(each[0]))
         Z.append(complex(float(each[4]), float(each[5])))
 
@@ -131,24 +140,24 @@ def readAutolab(filename):
 
 
 def readBioLogic(filename):
-    """ function for reading the .mpt file from Biologic
-        EC-lab software
+    """function for reading the .mpt file from Biologic
+    EC-lab software
 
-        Parameters
-        ----------
-        filename: string
-            Filename of .mpt file to extract impedance data from
+    Parameters
+    ----------
+    filename: string
+        Filename of .mpt file to extract impedance data from
 
-        Returns
-        -------
-        frequencies : np.ndarray
-            Array of frequencies
-        impedance : np.ndarray of complex numbers
-            Array of complex impedances
+    Returns
+    -------
+    frequencies : np.ndarray
+        Array of frequencies
+    impedance : np.ndarray of complex numbers
+        Array of complex impedances
 
-        """
+    """
 
-    with open(filename, 'r', encoding="latin-1") as input_file:
+    with open(filename, "r", encoding="latin-1") as input_file:
         lines = input_file.readlines()
 
     header_line = lines[1]
@@ -157,13 +166,13 @@ def readBioLogic(filename):
     number_header_lines = int(header_line.split(":")[1])
 
     # find the freq and Z columns
-    headers = lines[number_header_lines-1].split('\t')
+    headers = lines[number_header_lines - 1].split("\t")
 
-    freq_cols = [o for o, h in enumerate(headers) if h == 'freq/Hz']
-    ReZ_cols = [o for o, h in enumerate(headers) if h == 'Re(Z)/Ohm']
-    ImZ_cols = [o for o, h in enumerate(headers) if h == '-Im(Z)/Ohm']
+    freq_cols = [o for o, h in enumerate(headers) if h == "freq/Hz"]
+    ReZ_cols = [o for o, h in enumerate(headers) if h == "Re(Z)/Ohm"]
+    ImZ_cols = [o for o, h in enumerate(headers) if h == "-Im(Z)/Ohm"]
 
-    col_heads = ['freq/Hz', 'Re(Z)/Ohm', '-Im(Z)/Ohm']
+    col_heads = ["freq/Hz", "Re(Z)/Ohm", "-Im(Z)/Ohm"]
     for cols, ch in zip([freq_cols, ReZ_cols, ImZ_cols], col_heads):
         assert len(cols) > 0, f'"{ch}" not found in column headers'
 
@@ -174,17 +183,17 @@ def readBioLogic(filename):
     raw_data = lines[number_header_lines:]
     f, Z = [], []
     for line in raw_data:
-        each = line.split('\t')
+        each = line.split("\t")
         f.append(float(each[freq_col]))
 
         # MPT data format saves the imaginary portion as -Im(Z) not Im(Z)
-        Z.append(complex(float(each[ReZ_col]), -1*float(each[ImZ_col])))
+        Z.append(complex(float(each[ReZ_col]), -1 * float(each[ImZ_col])))
 
     return np.array(f), np.array(Z)
 
 
 def readParstat(filename):
-    """ function for reading the .txt file from Parstat
+    """function for reading the .txt file from Parstat
 
     Parameters
     ----------
@@ -200,7 +209,7 @@ def readParstat(filename):
 
     """
 
-    with open(filename, 'r') as input_file:
+    with open(filename, "r") as input_file:
         lines = input_file.readlines()
 
     raw_data = lines[1:]
@@ -215,7 +224,7 @@ def readParstat(filename):
 
 
 def readVersaStudio(filename):
-    """ function for reading the .PAR file from VersaStudio
+    """function for reading the .PAR file from VersaStudio
 
     Parameters
     ----------
@@ -231,7 +240,8 @@ def readVersaStudio(filename):
 
     """
     from re import split
-    with open(filename, 'r', encoding="utf8") as input_file:
+
+    with open(filename, "r", encoding="utf8") as input_file:
         lines = input_file.readlines()
 
     # List to track [segment index, segment start line, segment end line]
@@ -240,18 +250,16 @@ def readVersaStudio(filename):
     for i, line in enumerate(lines):
         if "Segments" in line:
             if not segments:
-                segments = [[int(j) for j in split(r'[=\n]', line)
-                            if j.isdigit()]]
+                segments = [[int(j) for j in split(r"[=\n]", line) if j.isdigit()]]
 
-            elif [int(j) for j in split(r'[=\n]', line) if j.isdigit()]:
-                segments.append([int(j) for j in split(r'[=\n]', line)
-                                if j.isdigit()])
+            elif [int(j) for j in split(r"[=\n]", line) if j.isdigit()]:
+                segments.append([int(j) for j in split(r"[=\n]", line) if j.isdigit()])
 
         if segments:
             for j in segments:
-                if '<Segment' + str(segments[j[0]][0]) + '>' in line:
+                if "<Segment" + str(segments[j[0]][0]) + ">" in line:
                     segments[j[0]].append(i)
-                if '</Segment' + str(segments[j[0]][0]) + '>' in line:
+                if "</Segment" + str(segments[j[0]][0]) + ">" in line:
                     segments[j[0]].append(i)
 
     # Started building for option of multiple segments,
@@ -269,11 +277,11 @@ def readVersaStudio(filename):
     #         f.append(float(each[9]))
     #         Z.append(complex(float(each[14]),float(each[15])))
 
-    raw_data = lines[segments[1][1]+4:segments[1][2]]
+    raw_data = lines[segments[1][1] + 4 : segments[1][2]]
     f, Z = [], []
 
     for line in raw_data:
-        each = line.split(',')
+        each = line.split(",")
         f.append(float(each[9]))
         Z.append(complex(float(each[14]), float(each[15])))
 
@@ -281,7 +289,7 @@ def readVersaStudio(filename):
 
 
 def readZPlot(filename):
-    """ function for reading the .z file from Scribner's ZPlot
+    """function for reading the .z file from Scribner's ZPlot
 
     Parameters
     ----------
@@ -297,7 +305,8 @@ def readZPlot(filename):
 
     """
     import re
-    with open(filename, 'r', encoding="utf8") as input_file:
+
+    with open(filename, "r", encoding="utf8") as input_file:
         lines = input_file.readlines()
 
     for i, line in enumerate(lines):
@@ -309,21 +318,21 @@ def readZPlot(filename):
             head_line = i
 
     try:
-        raw_data = lines[start_line+1:]
+        raw_data = lines[start_line + 1 :]
     except UnboundLocalError:
-        raw_data = lines[head_line+1:]
+        raw_data = lines[head_line + 1 :]
 
     f, Z = [], []
     for line in raw_data:
         # Can use this approach if we don't mind importing re module
-        each = re.split('\t|, ', line)
+        each = re.split("\t|, ", line)
         f.append(float(each[0]))
         Z.append(complex(float(each[4]), float(each[5])))
     return np.array(f), np.array(Z)
 
 
 def readPowerSuite(filename):
-    """ function for reading the .txt file from Parstat
+    """function for reading the .txt file from Parstat
 
     Parameters
     ----------
@@ -339,14 +348,14 @@ def readPowerSuite(filename):
 
     """
 
-    with open(filename, 'r') as input_file:
+    with open(filename, "r") as input_file:
         lines = input_file.readlines()
 
     raw_data = lines[1:]
     f, Z = [], []
     for line in raw_data:
         if not line.isspace():
-            freq, z_re, z_im = line.split('\t')
+            freq, z_re, z_im = line.split("\t")
             f.append(float(freq))
             Z.append(complex(float(z_re), float(z_im)))
 
@@ -354,7 +363,7 @@ def readPowerSuite(filename):
 
 
 def readCHInstruments(filename):
-    """ function for reading the .txt file from CHInstruments
+    """function for reading the .txt file from CHInstruments
 
     Parameters
     ----------
@@ -370,20 +379,20 @@ def readCHInstruments(filename):
 
     """
 
-    with open(filename, 'r') as input_file:
+    with open(filename, "r") as input_file:
         lines = input_file.readlines()
 
     # Locate the line where the data lives
     for i, line in enumerate(lines):
-        if line.startswith('Freq/Hz'):
+        if line.startswith("Freq/Hz"):
             # CH instruments has an empty space b/w header
             # and start of data line
-            start_line = i+2
+            start_line = i + 2
 
     raw_data = lines[start_line:]
     f, Z = [], []
     for line in raw_data:
-        each = line.split(',')
+        each = line.split(",")
         f.append(float(each[0]))
         Z.append(complex(float(each[1]), float(each[2])))
 
@@ -391,7 +400,7 @@ def readCHInstruments(filename):
 
 
 def readCSV(filename):
-    """ function for reading plain csv files
+    """function for reading plain csv files
 
     Parameters
     ----------
@@ -407,16 +416,16 @@ def readCSV(filename):
         Array of complex impedances
 
     """
-    data = np.genfromtxt(filename, delimiter=',')
+    data = np.genfromtxt(filename, delimiter=",")
 
     f = data[:, 0]
-    Z = data[:, 1] + 1j*data[:, 2]
+    Z = data[:, 1] + 1j * data[:, 2]
 
     return f, Z
 
 
 def saveCSV(filename, frequencies, impedances, **kwargs):
-    """ saves frequencies and impedances to a csv
+    """saves frequencies and impedances to a csv
 
     Parameters
     ----------
@@ -429,18 +438,20 @@ def saveCSV(filename, frequencies, impedances, **kwargs):
     kwargs :
         Keyword arguments passed to np.savetxt
     """
-    if not filename.endswith('.csv'):
-        filename += '.csv'
+    if not filename.endswith(".csv"):
+        filename += ".csv"
 
-    data = np.vstack([frequencies,
-                      np.real(impedances),
-                      np.imag(impedances),
-                      ]).T
+    data = np.vstack(
+        [
+            frequencies,
+            np.real(impedances),
+            np.imag(impedances),
+        ]
+    ).T
 
-    header = 'freq,Re(Z),Im(Z)'
+    header = "freq,Re(Z),Im(Z)"
 
-    np.savetxt(filename, data, delimiter=',',
-               header=header, **kwargs)
+    np.savetxt(filename, data, delimiter=",", header=header, **kwargs)
 
 
 def ignoreBelowX(frequencies, Z):
