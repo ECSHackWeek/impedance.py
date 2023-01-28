43dbe6c0e46b52d6e1dcd6fc68b989f357bb8a82
oslopanda <33810430+oslopanda@users.noreply.github.com>
2023-01-28 23:38:23 +0100

    Plotting update and a bug fix (#237)
    * Update visualization.py
    
    * Update visualization.py
    
    * Update circuits.py
    
    * Update impedance/models/circuits/circuits.py
    
    ---------
    
    Co-authored-by: Matt Murbach <mdmurbach@users.noreply.github.com>

--- impedance/visualization.py
+++ impedance/visualization.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 
-def plot_nyquist(Z, scale=1, units='Ohms', fmt='.-', ax=None, **kwargs):
+def plot_nyquist(Z, scale=1, units='Ohms', fmt='.-', ax=None, labelsize=20, ticksize=14, **kwargs):
     """ Plots impedance as a Nyquist plot using matplotlib
 
         Parameters
@@ -42,12 +42,12 @@ def plot_nyquist(Z, scale=1, units='Ohms', fmt='.-', ax=None, **kwargs):
 
     # Set the labels to -imaginary vs real
     ax.set_xlabel(r'$Z^{\prime}(\omega)$ ' +
-                  '$[{}]$'.format(units), fontsize=20)
+                  '$[{}]$'.format(units), fontsize=labelsize)
     ax.set_ylabel(r'$-Z^{\prime\prime}(\omega)$ ' +
-                  '$[{}]$'.format(units), fontsize=20)
+                  '$[{}]$'.format(units), fontsize=labelsize)
 
     # Make the tick labels larger
-    ax.tick_params(axis='both', which='major', labelsize=14)
+    ax.tick_params(axis='both', which='major', labelsize=ticksize)
 
     # Change the number of labels on each axis to five
     ax.locator_params(axis='x', nbins=5, tight=True)
@@ -69,7 +69,7 @@ def plot_nyquist(Z, scale=1, units='Ohms', fmt='.-', ax=None, **kwargs):
     return ax
 
 
-def plot_bode(f, Z, scale=1, units='Ohms', fmt='.-', axes=None, **kwargs):
+def plot_bode(f, Z, scale=1, units='Ohms', fmt='.-', axes=None, labelsize=20, ticksize=14, **kwargs):
     """ Plots impedance as a Bode plot using matplotlib
 
         Parameters
@@ -109,16 +109,16 @@ def plot_bode(f, Z, scale=1, units='Ohms', fmt='.-', axes=None, **kwargs):
 
     # Set the y-axis labels
     ax_mag.set_ylabel(r'$|Z(\omega)|$ ' +
-                      '$[{}]$'.format(units), fontsize=20)
-    ax_phs.set_ylabel(r'$-\phi_Z(\omega)$ ' + r'$[^o]$', fontsize=20)
+                      '$[{}]$'.format(units), fontsize=labelsize)
+    ax_phs.set_ylabel(r'$-\phi_Z(\omega)$ ' + r'$[^o]$', fontsize=labelsize)
 
     for ax in axes:
         # Set the frequency axes title and make log scale
-        ax.set_xlabel('f [Hz]', fontsize=20)
+        ax.set_xlabel('f [Hz]', fontsize=labelsize)
         ax.set_xscale('log')
 
         # Make the tick labels larger
-        ax.tick_params(axis='both', which='major', labelsize=14)
+        ax.tick_params(axis='both', which='major', labelsize=ticksize)
 
         # Change the number of labels on each axis to five
         ax.locator_params(axis='y', nbins=5, tight=True)
