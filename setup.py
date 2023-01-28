de732aba6ec2a0bd9fd00f8bb3d3ce4f27c56f82
Matt Murbach <matthew.murbach@gmail.com>
2023-01-28 12:42:55 -0800

    remove support for Python 3.6

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://impedancepy.readthedocs.io/en/latest/",
     packages=setuptools.find_packages(),
-    python_requires="~=3.6",
+    python_requires="~=3.7",
     install_requires=['altair>=3.0', 'matplotlib>=3.0',
                       'numpy>=1.14', 'scipy>=1.0'],
     classifiers=(
