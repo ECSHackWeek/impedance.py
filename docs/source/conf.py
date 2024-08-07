13badf42e5735fb03937e9724d28be5646c95a1b
Brian Gerwe <brian.s.gerwe@gmail.com>
2024-08-07 13:27:26 -0700

    update intersphinx_mapping

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -193,7 +193,7 @@ epub_exclude_files = ['search.html']
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'python': ('https://docs.python.org/', None)}
 
 # -- Options for todo extension ----------------------------------------------
 
