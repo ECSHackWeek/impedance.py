edc554127cc20d9d28eaecda214e5b6f5ce840b4
Adrian Usler <adrian.usler@googlemail.com>
2026-03-31 00:24:08 +0200

    Fix test: take frequencies out of circuit_elements dict

--- impedance/models/circuits/fitting.py
+++ impedance/models/circuits/fitting.py
@@ -235,11 +235,11 @@ def wrapCircuit(circuit, constants):
 
         """
 
-        circuit_elements["frequencies"] = frequencies
+        arg_dict = {**circuit_elements, "frequencies": frequencies}
         x = eval(buildCircuit(circuit, frequencies, *parameters,
                               constants=constants, eval_string='',
                               index=0)[0],
-                 circuit_elements)
+                 arg_dict)
         y_real = np.real(x)
         y_imag = np.imag(x)
 
@@ -443,4 +443,6 @@ def check_and_eval(element):
         raise ValueError(f'{element} not in ' +
                          f'allowed elements ({allowed_elements})')
     else:
-        return eval(element, circuit_elements)
+        dummy_frequency_array = np.array([1.])
+        arg_dict = {**circuit_elements, "frequencies": dummy_frequency_array}
+        return eval(element, arg_dict)
